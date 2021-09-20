4c33e2f9fb11bc775f0d4b3130fa451a491817c9
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-09-20 16:22:17 +0100

    test GET /categories happy path

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,3 +6,14 @@ const request = require("supertest")
 
 beforeEach(() => seed(testData))
 afterAll(() => db.end())
+
+describe("GET /api/categories", () => {
+  it("200: responds with an array of appropriate category objects", async () => {
+    const { body } = await request(app).get("/api/categories").expect(200)
+    expect(body.categories[0]).toMatchObject({
+      slug: "euro game",
+      description: "Abstact games that involve little luck",
+    })
+    expect(body.categories.length).toBe(4)
+  })
+})
