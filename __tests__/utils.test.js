0f9eb063931942f98efd3b467238bfd88409e41d
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-09-20 14:24:49 +0100

    test formatUserData()

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,16 +1,39 @@
-const { categoryData } = require("../db/data/test-data")
-const { formatCategoryData } = require("../db/utils/data-manipulation")
+const { categoryData, userData } = require("../db/data/test-data")
+const {
+  formatCategoryData,
+  formatUserData,
+} = require("../db/utils/data-manipulation")
 
-describe("#formatCategories", () => {
+describe("#formatCategoryData", () => {
   it("should return an empty array if one is passed", () => {
     expect(formatCategoryData([])).toEqual([])
   })
-  it("should return a nested array of values for a category object", () => {
+  it("should return a nested array of values for a nested category object", () => {
     expect(formatCategoryData(categoryData.slice(0, 1))).toEqual([
       ["euro game", "Abstact games that involve little luck"],
     ])
   })
   it("should handle arbitrary numbers of categories", () => {
     expect(formatCategoryData(categoryData).length).toBe(4)
+  })
+})
+
+describe.only("#formatUserData", () => {
+  it("should return an empty array if passed one", () => {
+    expect(formatUserData([])).toEqual([])
+  })
+
+  it("should return a nested array of values for a nested person object", () => {
+    expect(formatUserData(userData.slice(0, 1))).toEqual([
+      [
+        "mallionaire",
+        "haz",
+        "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+      ],
+    ])
+  })
+
+  it("should handle an arbitrary number of person objects", () => {
+    expect(formatUserData(userData).length).toBe(4)
   })
 })
