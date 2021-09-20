4d68a7cb583920b2aed72792f55a5bc9d39ee706
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-09-20 11:44:56 +0100

    import db module

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,9 @@
-const seed = (data) => {
-  const { categoryData, commentData, reviewData, userData } = data;
+const db = require("../connection")
+
+const seed = async (data) => {
+  const { categoryData, commentData, reviewData, userData } = data
   // 1. create tables
   // 2. insert data
-};
+}
 
-module.exports = seed;
+module.exports = seed
