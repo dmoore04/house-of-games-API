433702dd0e3f6a2e556717c3c7cf08c496d88830
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-09-20 14:24:32 +0100

    implement formatUserData()

--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -2,3 +2,8 @@ exports.formatCategoryData = (categories) => {
   if (!categories.length) return []
   return categories.map((category) => [category.slug, category.description])
 }
+
+exports.formatUserData = (users) => {
+  if (!users.length) return []
+  return users.map((user) => [user.username, user.name, user.avatar_url])
+}
