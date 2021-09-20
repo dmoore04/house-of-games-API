601eb0ce1f7550b376331c4e4e9448fc73707b90
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-09-20 14:32:44 +0100

    implement formatReviewData()

--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -7,3 +7,17 @@ exports.formatUserData = (users) => {
   if (!users.length) return []
   return users.map((user) => [user.username, user.name, user.avatar_url])
 }
+
+exports.formatReviewData = (reviews) => {
+  if (!reviews.length) return []
+  return reviews.map((review) => [
+    review.title,
+    review.designer,
+    review.owner,
+    review.review_img_url,
+    review.review_body,
+    review.category,
+    review.created_at,
+    review.votes,
+  ])
+}
