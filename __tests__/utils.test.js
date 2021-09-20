c6c52e239b0682dd2429990f365dac7054df7649
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-09-20 15:01:15 +0100

    test formatCommentData()

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,65 +1,93 @@
-const { categoryData, userData, reviewData } = require("../db/data/test-data")
+const {
+  categoryData,
+  userData,
+  reviewData,
+  commentData,
+} = require("../db/data/test-data")
+
 const {
   formatCategoryData,
   formatUserData,
   formatReviewData,
+  formatCommentData,
 } = require("../db/utils/data-manipulation")
 
-describe("#formatCategoryData", () => {
-  it("should return an empty array if one is passed", () => {
-    expect(formatCategoryData([])).toEqual([])
-  })
-  it("should return a nested array of values for a nested category object", () => {
-    expect(formatCategoryData(categoryData.slice(0, 1))).toEqual([
-      ["euro game", "Abstact games that involve little luck"],
-    ])
-  })
-  it("should handle arbitrary numbers of categories", () => {
-    expect(formatCategoryData(categoryData).length).toBe(4)
-  })
-})
-
-describe("#formatUserData", () => {
-  it("should return an empty array if passed one", () => {
-    expect(formatUserData([])).toEqual([])
+describe("Data Formatting", () => {
+  test("each function returns an empty array if one is passed", () => {
+    const functions = [
+      formatCategoryData,
+      formatCommentData,
+      formatReviewData,
+      formatUserData,
+    ]
+    functions.forEach((func) => {
+      expect(func([])).toEqual([])
+    })
   })
 
-  it("should return a nested array of values for a nested person object", () => {
-    expect(formatUserData(userData.slice(0, 1))).toEqual([
-      [
-        "mallionaire",
-        "haz",
-        "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
-      ],
-    ])
+  describe("#formatCategoryData", () => {
+    it("should return a nested array of values for a nested category object", () => {
+      expect(formatCategoryData(categoryData.slice(0, 1))).toEqual([
+        ["euro game", "Abstact games that involve little luck"],
+      ])
+    })
+    it("should handle arbitrary numbers of categories", () => {
+      expect(formatCategoryData(categoryData).length).toBe(4)
+    })
   })
 
-  it("should handle an arbitrary number of person objects", () => {
-    expect(formatUserData(userData).length).toBe(4)
-  })
-})
+  describe("#formatUserData", () => {
+    it("should return a nested array of values for a nested person object", () => {
+      expect(formatUserData(userData.slice(0, 1))).toEqual([
+        [
+          "mallionaire",
+          "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+          "haz",
+        ],
+      ])
+    })
 
-describe.only("#formatReviewData", () => {
-  it("should return an empty array if passed one", () => {
-    expect(formatReviewData([])).toEqual([])
-  })
-
-  it("should return a nested array of values for a nested person object", () => {
-    expect(formatReviewData(reviewData.slice(0, 1))).toEqual([
-      [
-        "Agricola",
-        "Uwe Rosenberg",
-        "mallionaire",
-        "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
-        "Farmyard fun!",
-        "euro game",
-        new Date(1610964020514),
-        1,
-      ],
-    ])
+    it("should handle an arbitrary number of person objects", () => {
+      expect(formatUserData(userData).length).toBe(4)
+    })
   })
 
-  it("should handle an arbitrary number of person objects", () => {
-    expect(formatReviewData(reviewData).length).toBe(13)
+  describe("#formatReviewData", () => {
+    it("should return a nested array of values for a nested person object", () => {
+      expect(formatReviewData(reviewData.slice(0, 1))).toEqual([
+        [
+          "Agricola",
+          "Farmyard fun!",
+          "Uwe Rosenberg",
+          "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+          1,
+          "euro game",
+          "mallionaire",
+          new Date(1610964020514),
+        ],
+      ])
+    })
+
+    it("should handle an arbitrary number of person objects", () => {
+      expect(formatReviewData(reviewData).length).toBe(13)
+    })
+  })
+
+  describe("#formatCommentData", () => {
+    it("should return a nested array of values for a nested comment object", () => {
+      expect(formatCommentData(commentData.slice(0, 1))).toEqual([
+        [
+          "bainesface",
+          2,
+          16,
+          new Date(1511354613389),
+          "I loved this game too!",
+        ],
+      ])
+    })
+
+    it("should handle arbitrary numbers of comments", () => {
+      expect(formatCommentData(commentData).length).toBe(6)
+    })
   })
 })
