2bc1174dfdff94b68960797762958182a47c8938
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-09-21 12:12:46 +0100

    add json middleware

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const apiRouter = require("./routes/api.router")
 
 const app = express()
 
+app.use(express.json())
+
 app.use("/api", apiRouter)
+
+app.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).send(err.msg)
+})
 
 module.exports = app
