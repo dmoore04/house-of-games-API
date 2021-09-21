c9be175451a43676263342fca396dbd30ad4fb61
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-09-21 11:41:41 +0100

    improve GET /categories test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,10 +10,19 @@ afterAll(() => db.end())
 describe("GET /api/categories", () => {
   it("200: responds with an array of appropriate category objects", async () => {
     const { body } = await request(app).get("/api/categories").expect(200)
+
+    expect(body.categories.length).toBe(4)
+
     expect(body.categories[0]).toMatchObject({
       slug: "euro game",
       description: "Abstact games that involve little luck",
     })
-    expect(body.categories.length).toBe(4)
+
+    body.categories.forEach((category) => {
+      expect(category).toMatchObject({
+        slug: expect.any(String),
+        description: expect.any(String),
+      })
+    })
   })
 })
