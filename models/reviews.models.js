fb32b714a1bc763a95d0735a2476196f1985c6dd
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-09-21 15:52:19 +0100

    implement GET /reviews sort_by query

--- models/reviews.models.js
+++ models/reviews.models.js
@@ -1,12 +1,18 @@
 const db = require("../db/connection")
+const format = require("pg-format")
 
-exports.selectReviews = async () => {
-  const queryStr = `
+exports.selectReviews = async (sort_by = "created_at") => {
+  const queryStr = format(
+    `
   SELECT reviews.*, COUNT(comments.review_id)::INT AS comment_count
   FROM reviews
     LEFT JOIN comments ON reviews.review_id = comments.review_id
   GROUP BY reviews.review_id
-  `
+  ORDER BY %I
+  `,
+    sort_by
+  )
   const reviews = await db.query(queryStr)
+  //console.log(reviews.rows)
   return reviews.rows
 }
