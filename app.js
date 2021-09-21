942ff0931e0ef2d19f4fe7cfd6c5ef173c7add87
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-09-21 16:09:05 +0100

    handle bad route 404 errors

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.use(express.json())
 
 app.use("/api", apiRouter)
 
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "Route not found" })
+})
+
 app.use((err, req, res, next) => {
   console.log(err)
   res.status(500).send(err.msg)
