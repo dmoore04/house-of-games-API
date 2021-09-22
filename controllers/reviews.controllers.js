7c72398adf09079cc0b0b417cfddc6de5214e4be
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-09-22 12:29:03 +0100

    implement GET /reviews category query

--- controllers/reviews.controllers.js
+++ controllers/reviews.controllers.js
@@ -2,8 +2,8 @@ const { selectReviews } = require("../models/reviews.models")
 
 exports.sendReviews = async (req, res, next) => {
   try {
-    const { sort_by, order } = req.query
-    const reviews = await selectReviews(sort_by, order)
+    const { sort_by, order, category } = req.query
+    const reviews = await selectReviews(sort_by, order, category)
     res.status(200).send({ reviews })
   } catch (err) {
     next(err)
--- models/reviews.models.js
+++ models/reviews.models.js
@@ -1,17 +1,30 @@
 const db = require("../db/connection")
 const format = require("pg-format")
 
-exports.selectReviews = async (sort_by = "created_at", order = "desc") => {
-  const queryStr = format(
-    `
+exports.selectReviews = async (
+  sort_by = "created_at",
+  order = "desc",
+  category
+) => {
+  let queryStr = `
   SELECT reviews.*, COUNT(comments.review_id)::INT AS comment_count
   FROM reviews
     LEFT JOIN comments ON reviews.review_id = comments.review_id
+  `
+
+  const queryValues = []
+  if (category) {
+    queryStr += `WHERE category = $1`
+    queryValues.push(category)
+  }
+
+  queryStr += format(
+    `
   GROUP BY reviews.review_id
-  ORDER BY %I ${order} 
-  `,
-    sort_by
+  ORDER BY %I ${order}`,
+    [sort_by]
   )
-  const reviews = await db.query(queryStr)
+
+  const reviews = await db.query(queryStr, queryValues)
   return reviews.rows
 }
