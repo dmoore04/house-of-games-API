60db6c2cc1b838fbb84749ab895ebed9e3d09347
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-09-22 14:56:33 +0100

    refactor error handler

--- errors/index.js
+++ errors/index.js
@@ -1,4 +1,4 @@
-exports.handlePSQL400Errors = (err, req, res, next) => {
+exports.handlePSQL = (err, req, res, next) => {
   const codeMap = {
     42703: "Invalid query value",
     "22P02": "Bad request",
@@ -6,4 +6,17 @@ exports.handlePSQL400Errors = (err, req, res, next) => {
 
   const msg = codeMap[err.code]
   msg ? res.status(400).send({ msg }) : next(err)
+}
+
+exports.handleCustom = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg })
+  } else {
+    next(err)
+  }
+}
+
+exports.handle500 = (err, req, res, next) => {
+  console.log(err)
+  res.status(500).send(err.msg)
 }
