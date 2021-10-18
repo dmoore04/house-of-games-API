691efd44145c98006c220b34516c07e7c6404f17
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-10-18 11:59:40 +0100

    add cors middlewear

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const apiRouter = require("./routes/api.router")
 const errors = require("./errors")
+const cors = require("cors")
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use("/api", apiRouter)
